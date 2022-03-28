64b73c23d8404abbcd669471bcbb6db2903c53fe
Artem Mikhailov <mega.artemm@bk.ru>
2022-03-28 15:10:55 +0300

    icon

---
+++ app/src/main/java/ru/a/o/mikhailov/bmch/ChangeExercisesActivity.kt
@@ -0,0 +1,11 @@
+package ru.a.o.mikhailov.bmch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangeExercisesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_exercises)
+    }
+}
