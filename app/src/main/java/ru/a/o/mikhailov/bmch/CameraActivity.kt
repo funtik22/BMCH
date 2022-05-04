85543b6e8f669666b9fad5183613011d588411b2
Artem Mikhailov <mega.artemm@bk.ru>
2022-05-04 16:19:02 +0300

    Pose detection https://github.com/nekdenis/camera_workshop

--- app/src/main/java/ru/a/o/mikhailov/bmch/CameraActivity.kt
+++ app/src/main/java/ru/a/o/mikhailov/bmch/CameraActivity.kt
@@ -1,19 +1,50 @@
 package ru.a.o.mikhailov.bmch
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
 import android.widget.Toast
+import androidx.compose.runtime.*
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cameraswitch
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.android.gms.tasks.TaskExecutors
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.mlkit.common.MlKitException
+import com.google.mlkit.vision.pose.Pose
+import com.google.mlkit.vision.pose.PoseLandmark
+
 
 class CameraActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +54,7 @@ class CameraActivity : ComponentActivity() {
         } else {
             requestPermission()
         }
+
     }
 
     private fun permissionGranted() =
@@ -37,7 +69,6 @@ class CameraActivity : ComponentActivity() {
         )
     }
 
-    @Deprecated("Deprecated in Java")
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String?>,
@@ -55,18 +86,301 @@ class CameraActivity : ComponentActivity() {
 
     private fun initView() {
         setContent {
-                    Greeting("Android")
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    var lens by remember { mutableStateOf(CameraSelector.LENS_FACING_FRONT) }
+                    CameraPreview(
+                        cameraLens = lens
+                    )
+                    Controls(
+                        onLensChange = { lens = switchLens(lens) }
+                    )
+                }
+
+        }
+    }
+
+    @Composable
+    fun CameraPreview(
+        modifier: Modifier = Modifier,
+        cameraLens: Int
+    ) {
+        val lifecycleOwner = LocalLifecycleOwner.current
+        val context = LocalContext.current
+        var sourceInfo by remember { mutableStateOf(SourceInfo(10, 10, false)) }
+        var detectedPose by remember { mutableStateOf<Pose?>(null) }
+        val previewView = remember { PreviewView(context) }
+        val cameraProvider = remember(sourceInfo, cameraLens) {
+            ProcessCameraProvider.getInstance(context)
+                .configureCamera(
+                    previewView, lifecycleOwner, cameraLens, context,
+                    setSourceInfo = { sourceInfo = it },
+                    onPoseDetected = { detectedPose = it }
+                )
+        }
+
+        BoxWithConstraints(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            with(LocalDensity.current) {
+                Box(
+                    modifier = Modifier
+                        .size(
+                            height = sourceInfo.height.toDp(),
+                            width = sourceInfo.width.toDp()
+                        )
+                        .scale(
+                            calculateScale(
+                                constraints,
+                                sourceInfo,
+                                PreviewScaleType.CENTER_CROP
+                            )
+                        )
+                )
+                {
+                    CameraPreview(previewView)
+                    DetectedPose(pose = detectedPose, sourceInfo = sourceInfo)
                 }
             }
-}
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-        Greeting("Android")
+private fun CameraPreview(previewView: PreviewView) {
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = {
+            previewView.apply {
+                this.scaleType = PreviewView.ScaleType.FILL_CENTER
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                // Preview is incorrectly scaled in Compose on some devices without this
+                implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+            }
+
+            previewView
+        })
 }
 
+
+@Composable
+fun DetectedPose(
+    pose: Pose?,
+    sourceInfo: SourceInfo
+) {
+    if (pose != null) {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val strokeWidth = 1.dp.toPx()
+            val whitePaint = SolidColor(Color.White)
+            val leftPaint = SolidColor(Color.Green)
+            val rightPaint = SolidColor(Color.Yellow)
+
+            val needToMirror = sourceInfo.isImageFlipped
+            val leftShoulder = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER)
+            val rightShoulder = pose.getPoseLandmark(PoseLandmark.RIGHT_SHOULDER)
+            val leftElbow = pose.getPoseLandmark(PoseLandmark.LEFT_ELBOW)
+            val rightElbow = pose.getPoseLandmark(PoseLandmark.RIGHT_ELBOW)
+            val leftWrist = pose.getPoseLandmark(PoseLandmark.LEFT_WRIST)
+            val rightWrist = pose.getPoseLandmark(PoseLandmark.RIGHT_WRIST)
+            val leftHip = pose.getPoseLandmark(PoseLandmark.LEFT_HIP)
+            val rightHip = pose.getPoseLandmark(PoseLandmark.RIGHT_HIP)
+            val leftKnee = pose.getPoseLandmark(PoseLandmark.LEFT_KNEE)
+            val rightKnee = pose.getPoseLandmark(PoseLandmark.RIGHT_KNEE)
+            val leftAnkle = pose.getPoseLandmark(PoseLandmark.LEFT_ANKLE)
+            val rightAnkle = pose.getPoseLandmark(PoseLandmark.RIGHT_ANKLE)
+
+            val leftPinky = pose.getPoseLandmark(PoseLandmark.LEFT_PINKY)
+            val rightPinky = pose.getPoseLandmark(PoseLandmark.RIGHT_PINKY)
+            val leftIndex = pose.getPoseLandmark(PoseLandmark.LEFT_INDEX)
+            val rightIndex = pose.getPoseLandmark(PoseLandmark.RIGHT_INDEX)
+            val leftThumb = pose.getPoseLandmark(PoseLandmark.LEFT_THUMB)
+            val rightThumb = pose.getPoseLandmark(PoseLandmark.RIGHT_THUMB)
+            val leftHeel = pose.getPoseLandmark(PoseLandmark.LEFT_HEEL)
+            val rightHeel = pose.getPoseLandmark(PoseLandmark.RIGHT_HEEL)
+            val leftFootIndex = pose.getPoseLandmark(PoseLandmark.LEFT_FOOT_INDEX)
+            val rightFootIndex = pose.getPoseLandmark(PoseLandmark.RIGHT_FOOT_INDEX)
+
+            fun drawLine(
+                startLandmark: PoseLandmark?,
+                endLandmark: PoseLandmark?,
+                paint: Brush
+            ) {
+                if (startLandmark != null && endLandmark != null) {
+                    val startX =
+                        if (needToMirror) size.width - startLandmark.position.x else startLandmark.position.x
+                    val startY = startLandmark.position.y
+                    val endX =
+                        if (needToMirror) size.width - endLandmark.position.x else endLandmark.position.x
+                    val endY = endLandmark.position.y
+                    drawLine(
+                        brush = paint,
+                        start = Offset(startX, startY),
+                        end = Offset(endX, endY),
+                        strokeWidth = strokeWidth,
+                    )
+                }
+            }
+
+            drawLine(leftShoulder, rightShoulder, whitePaint)
+            drawLine(leftHip, rightHip, whitePaint)
+            // Left body
+            drawLine(leftShoulder, leftElbow, leftPaint)
+            drawLine(leftElbow, leftWrist, leftPaint)
+            drawLine(leftShoulder, leftHip, leftPaint)
+            drawLine(leftHip, leftKnee, leftPaint)
+            drawLine(leftKnee, leftAnkle, leftPaint)
+            drawLine(leftWrist, leftThumb, leftPaint)
+            drawLine(leftWrist, leftPinky, leftPaint)
+            drawLine(leftWrist, leftIndex, leftPaint)
+            drawLine(leftIndex, leftPinky, leftPaint)
+            drawLine(leftAnkle, leftHeel, leftPaint)
+            drawLine(leftHeel, leftFootIndex, leftPaint)
+            // Right body
+            drawLine(rightShoulder, rightElbow, rightPaint)
+            drawLine(rightElbow, rightWrist, rightPaint)
+            drawLine(rightShoulder, rightHip, rightPaint)
+            drawLine(rightHip, rightKnee, rightPaint)
+            drawLine(rightKnee, rightAnkle, rightPaint)
+            drawLine(rightWrist, rightThumb, rightPaint)
+            drawLine(rightWrist, rightPinky, rightPaint)
+            drawLine(rightWrist, rightIndex, rightPaint)
+            drawLine(rightIndex, rightPinky, rightPaint)
+            drawLine(rightAnkle, rightHeel, rightPaint)
+            drawLine(rightHeel, rightFootIndex, rightPaint)
+        }
+    }
+}
+
+@Composable
+fun Controls(
+    onLensChange: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = 24.dp),
+        contentAlignment = Alignment.BottomCenter,
+    ) {
+        Button(
+            onClick = onLensChange,
+            modifier = Modifier.wrapContentSize()
+        ) { Icon(Icons.Filled.Cameraswitch, contentDescription = "Switch camera") }
+    }
+}
+
+private fun ListenableFuture<ProcessCameraProvider>.configureCamera(
+    previewView: PreviewView,
+    lifecycleOwner: LifecycleOwner,
+    cameraLens: Int,
+    context: Context,
+    setSourceInfo: (SourceInfo) -> Unit,
+    onPoseDetected: (Pose) -> Unit
+): ListenableFuture<ProcessCameraProvider> {
+    addListener({
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(cameraLens).build()
+
+        val preview = androidx.camera.core.Preview.Builder()
+            .build()
+            .apply {
+                setSurfaceProvider(previewView.surfaceProvider)
+            }
+
+        val analysis =
+            bindAnalysisUseCase(cameraLens, setSourceInfo, onPoseDetected)
+        try {
+            get().apply {
+                unbindAll()
+                bindToLifecycle(lifecycleOwner, cameraSelector, preview)
+                bindToLifecycle(lifecycleOwner, cameraSelector, analysis)
+            }
+        } catch (exc: Exception) {
+            TODO("process errors")
+        }
+    }, ContextCompat.getMainExecutor(context))
+    return this
+}
+
+private fun switchLens(lens: Int) = if (CameraSelector.LENS_FACING_FRONT == lens) {
+    CameraSelector.LENS_FACING_BACK
+} else {
+    CameraSelector.LENS_FACING_FRONT
+}
+
+
+private fun bindAnalysisUseCase(
+    lens: Int,
+    setSourceInfo: (SourceInfo) -> Unit,
+    onPoseDetected: (Pose) -> Unit
+): ImageAnalysis? {
+
+    val poseProcessor = try {
+        PoseDetectorProcessor()
+    } catch (e: Exception) {
+        Log.e("CAMERA", "Can not create pose processor", e)
+        return null
+    }
+    val builder = ImageAnalysis.Builder()
+    val analysisUseCase = builder.build()
+
+    var sourceInfoUpdated = false
+
+    analysisUseCase.setAnalyzer(
+        TaskExecutors.MAIN_THREAD
+    ) { imageProxy: ImageProxy ->
+        if (!sourceInfoUpdated) {
+            setSourceInfo(obtainSourceInfo(lens, imageProxy))
+            sourceInfoUpdated = true
+        }
+        try {
+//                faceProcessor.processImageProxy(imageProxy, onFacesDetected)
+            poseProcessor.processImageProxy(imageProxy, onPoseDetected)
+        } catch (e: MlKitException) {
+            Log.e(
+                "CAMERA", "Failed to process image. Error: " + e.localizedMessage
+            )
+        }
+    }
+    return analysisUseCase
+}
+
+private fun obtainSourceInfo(lens: Int, imageProxy: ImageProxy): SourceInfo {
+    val isImageFlipped = lens == CameraSelector.LENS_FACING_FRONT
+    val rotationDegrees = imageProxy.imageInfo.rotationDegrees
+    return if (rotationDegrees == 0 || rotationDegrees == 180) {
+        SourceInfo(
+            height = imageProxy.height, width = imageProxy.width, isImageFlipped = isImageFlipped
+        )
+    } else {
+        SourceInfo(
+            height = imageProxy.width, width = imageProxy.height, isImageFlipped = isImageFlipped
+        )
+    }
+}
+
+private fun calculateScale(
+    constraints: Constraints,
+    sourceInfo: SourceInfo,
+    scaleType: PreviewScaleType
+): Float {
+    val heightRatio = constraints.maxHeight.toFloat() / sourceInfo.height
+    val widthRatio = constraints.maxWidth.toFloat() / sourceInfo.width
+    return when (scaleType) {
+        PreviewScaleType.FIT_CENTER -> kotlin.math.min(heightRatio, widthRatio)
+        PreviewScaleType.CENTER_CROP -> kotlin.math.max(heightRatio, widthRatio)
+    }
+}
+
+data class SourceInfo(
+    val width: Int,
+    val height: Int,
+    val isImageFlipped: Boolean,
+)
+
+private enum class PreviewScaleType {
+    FIT_CENTER,
+    CENTER_CROP
+}
